99906fd8f0213e2985a22a1501392598f9ca246c
NereaPardo91 <n.pardonavarro1991@gmail.com>
2016-10-09 22:58:53 +0200

    lectura correcta

--- karaoke.py
+++ karaoke.py
@@ -1,28 +1,28 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-import smallsmilhandler
 import sys
+from smallsmilhandler import SmallSMILHandler
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 
 
-class Karaoke(smallsmilhandler.SmallSMILHandler):
 
-	def __init__ (self, fichero): 
-
-		parser = make_parser()
-		kHandler = smallsmilhandler.SmallSMILHandler()
-		parser.setContentHandler(kHandler)
-		parser.parse(open(fichero))
-		self.misDatos = kHandler.get_tags()
 
 
 if __name__ == "__main__":
 
+	parser = make_parser()
+	sHandler = SmallSMILHandler()
+	parser.setContentHandler(sHandler)
 	try:
 		fichero = sys.argv[1]
 
 	except IndexError:
 		sys.exit("Usage: python3 karaoke.py file.smil")
-    
+
+	parser.parse(open(fichero))
+	misDatos = sHandler.get_tags()
+	print(misDatos)
+
+
    
