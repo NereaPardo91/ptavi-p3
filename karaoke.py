b8387f2adc94ee3a8eb497d64ade8b4e4998cfe0
NereaPardo91 <n.pardonavarro1991@gmail.com>
2016-10-10 16:47:01 +0200

    json acabado

--- karaoke.py
+++ karaoke.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
+import json
 import sys
 from smallsmilhandler import SmallSMILHandler
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 
+
+def to_json(misDatos, fichero):
+	fjson = open(fichero[:-4] + 'json', 'w')
+	json.dump(misDatos, fjson)
 
 if __name__ == "__main__":
 
@@ -22,11 +27,16 @@ if __name__ == "__main__":
 	misDatos = sHandler.get_tags()
 
 	for elems in misDatos:
+
 		etiq = elems['tag']  #cojo mi etiqueta tag para que sea lo primero que se imprime
 		del elems['tag']	#borro tag de mi dicc para que no se repita al final
 		text = (etiq +'\t')
+
 		for atributos in elems:
-			text = text+ '\t' + atributos + '= "' + elems[atributos] + '"' +'\t'
+			text = text + '\t' + atributos + '= "' + elems[atributos] + '"' + '\t'
 		print(text)
+
+
+	to_json(misDatos, fichero)
 
 	 
