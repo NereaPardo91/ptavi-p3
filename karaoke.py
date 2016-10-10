8cd43528ead2f4f5782cf25252dadf3b7c1aa9bd
NereaPardo91 <n.pardonavarro1991@gmail.com>
2016-10-10 20:46:58 +0200

    Sin pep8 con error en str

--- karaoke.py
+++ karaoke.py
@@ -7,46 +7,52 @@ from smallsmilhandler import SmallSMILHandler
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 
-def to_json(misDatos, fichero):
-	fjson = open(fichero[:-4] + 'json', 'w')
-	json.dump(misDatos, fjson)
+class karaokeLocal():
 
-def local(misDatos):
-	for elems in misDatos:
-		for atributos in elems:
-			if elems[atributos].startswith('http://'): #si el valor de mi clave empieza por http...
-				name = elems[atributos].split('/')[-1] #cogeme a partir de la ultima /
-				urllib.request.urlretrieve(elems[atributos], name) #descarga del "archivo"
-				elems[atributos] = name #quedate con logo.gif
+	def init(self, fichero):
+		parser = make_parser()
+		self.sHandler = SmallSMILHandler()
+		parser.setContentHandler(self.sHandler)
+		parser.parse(open(fichero))
+		self.misDatos = self.sHandler.get_tags()
+
+	def __str__(self):
+		for elems in self.misDatos:
+			etiq = elems['tag']  #cojo mi etiqueta tag para que sea lo primero que se imprime
+			del elems['tag']	#borro tag de mi dicc para que no se repita al final
+			text = (etiq +'\t')
+
+			for atributos in elems:
+				text = text + '\t' + atributos + '= "' + elems[atributos] + '"' + '\t'
+			print(text)
+
+	def to_json(self, fichero):
+		fjson = open(fichero[:-4] + 'json', 'w')
+		json.dump(self.misDatos, fjson)
+
+	def do_local(self):
+		for elems in self.misDatos:
+			for atributos in elems:
+				if elems[atributos].startswith('http://'): #si el valor de mi clave empieza por http...
+					name = elems[atributos].split('/')[-1] #cogeme a partir de la ultima /
+					urllib.request.urlretrieve(elems[atributos], name) #descarga del "archivo"
+					elems[atributos] = name #quedate con logo.gif
 
 if __name__ == "__main__":
 
-	parser = make_parser()
-	sHandler = SmallSMILHandler()
-	parser.setContentHandler(sHandler)
+	karaoke = karaokeLocal()
 
 	try:
 		fichero = sys.argv[1]
-
 	except IndexError:
 		sys.exit("Usage: python3 karaoke.py file.smil")
-
-	parser.parse(open(fichero))
-	misDatos = sHandler.get_tags()
-
-	local(misDatos) #lo llamo antes del for para que se asigne el nuevo valor bien
-
-	for elems in misDatos:
-
-		etiq = elems['tag']  #cojo mi etiqueta tag para que sea lo primero que se imprime
-		del elems['tag']	#borro tag de mi dicc para que no se repita al final
-		text = (etiq +'\t')
-
-		for atributos in elems:
-			text = text + '\t' + atributos + '= "' + elems[atributos] + '"' + '\t'
-		print(text)
+	
+	karaoke.init(fichero)
+	karaoke.__str__()
+	karaoke.to_json(sys.argv[1])
+	karaoke.do_local()
+	karaoke.to_json('local')
+	karaoke.__str__()
 
 
-	to_json(misDatos, fichero)
 
-	 
