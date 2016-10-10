cd21096e6f9374bcfa9b6d356dbc2cf9bf0ce292
NereaPardo91 <n.pardonavarro1991@gmail.com>
2016-10-10 18:53:10 +0200

    Apartado 5 terminado

--- karaoke.py
+++ karaoke.py
@@ -2,14 +2,22 @@
 # -*- coding: utf-8 -*-
 import json
 import sys
+import urllib.request
 from smallsmilhandler import SmallSMILHandler
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 
-
 def to_json(misDatos, fichero):
 	fjson = open(fichero[:-4] + 'json', 'w')
 	json.dump(misDatos, fjson)
+
+def local(misDatos):
+	for elems in misDatos:
+		for atributos in elems:
+			if elems[atributos].startswith('http://'): #si el valor de mi clave empieza por http...
+				name = elems[atributos].split('/')[-1] #cogeme a partir de la ultima /
+				urllib.request.urlretrieve(elems[atributos], name) #descarga del "archivo"
+				elems[atributos] = name #quedate con logo.gif
 
 if __name__ == "__main__":
 
@@ -25,6 +33,8 @@ if __name__ == "__main__":
 
 	parser.parse(open(fichero))
 	misDatos = sHandler.get_tags()
+
+	local(misDatos) #lo llamo antes del for para que se asigne el nuevo valor bien
 
 	for elems in misDatos:
 
